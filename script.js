c3b1bb54e35436927cc384d4a69e2cb0eccbf56d
Jeff <jeff_lim408@icloud.com>
2023-03-16 21:57:01 -0500

    message

--- script.js
+++ script.js
@@ -17,22 +17,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+function newTitle() {
+  document.getElementById('main-title').innerText = 'Toretto.';
+}
+newTitle();
 
   // Part 2
-
+this.body.style.backgroundColor = "#2F4F4F"
 
   // Part 3
-
+document.getElementById("favorite-things").children[5].remove();
 
   // Part 4
-
+function makeTitleBigger() {
+  let specialItems = document.getElementsByClassName('special-title');
+  for (let i = 0; i < specialItems.length; i++) {
+    specialItems[i].style.fontSize = '2rem';
+  }
+}
+  makeTitleBigger()
 
   // Part 5
-
+document.getElementById('past-races').children[3].remove();
 
   // Part 6
-
+function addRace(city) {
+  let newLi = (document.createElement('li').innerText = city);
+  document.getElementById('past-races').append(newLi);
+}
+addRace(city);
 
   // Part 7
 
